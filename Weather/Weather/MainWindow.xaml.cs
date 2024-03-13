853146308ffb2bfdc9cfb7b5d3607aba428495d9
MoadJawabreh <moadjawbreh2001@yahoo.com>
2024-03-13 13:28:09 +0300

    UI and Api decided

--- Weather/Weather/MainWindow.xaml.cs
+++ Weather/Weather/MainWindow.xaml.cs
@@ -1,20 +1,10 @@
-﻿using System.Net.Http;
-using System.Text;
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Weather
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
 		private const string apiKey = "4b8f491d04a1db4cebb5b9bc5ce693bb";
@@ -42,58 +32,55 @@ namespace Weather
 
 		private async Task<string> GetWeatherData(double longitude, double latitude)
 		{
-			using (var client = new HttpClient())
+			string weatherInfo = "";
+
+			for (int height = 0; height <= 200; height += 20)
 			{
-				var request = new HttpRequestMessage
+				using (var client = new HttpClient())
 				{
-					Method = HttpMethod.Get,
-					RequestUri = new Uri($"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={apiKey}&units=metric")
-				};
+					var request = new HttpRequestMessage
+					{
+						Method = HttpMethod.Get,
+						RequestUri = new Uri($"https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={apiKey}&units=metric&height={height}")
+					};
 
-				HttpResponseMessage response = await client.SendAsync(request);
+					HttpResponseMessage response = await client.SendAsync(request);
 
-				if (response.IsSuccessStatusCode)
-				{
-					string responseBody = await response.Content.ReadAsStringAsync();
-					dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+					if (response.IsSuccessStatusCode)
+					{
+						string responseBody = await response.Content.ReadAsStringAsync();
+						dynamic data = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
 
-					double windSpeed = data.wind.speed;
-					double windDirectionDegrees = data.wind.deg;
-					string weatherDescription = data.weather[0].description;
-					string windDirection = ConvertDegreesToCardinal(windDirectionDegrees);
+						double temperature = data.main.temp;
+						double windSpeed = data.wind.speed;
+						double windDirectionDegrees = (double)data.wind.deg; // Ensure wind direction is parsed as double
 
-					return $"Weather at latitude {latitude}, longitude {longitude}:\nWeather Description: {weatherDescription}\nWind Speed: {windSpeed} m/s\nWind Direction: {windDirection}";
-				}
-				else
-				{
-					return $"Failed to retrieve weather data. Status code: {response.StatusCode}";
+						string weatherDescription = data.weather[0].description;
+						string windDirection = ConvertDegreesToCardinal(windDirectionDegrees);
+
+						weatherInfo += $"Height: {height} meters\nTemperature: {temperature}°C\nWind Speed: {windSpeed} m/s\nWind Direction: {windDirection}\nWeather Description: {weatherDescription}\n\n";
+					}
+					else
+					{
+						return $"Failed to retrieve weather data. Status code: {response.StatusCode}";
+					}
 				}
 			}
+
+			return weatherInfo;
 		}
 
 		private string ConvertDegreesToCardinal(double degrees)
 		{
 			string[] cardinalDirections = {
-	"North",
-	"North-Northeast",
-	"Northeast",
-	"East-Northeast",
-	"East",
-	"East-Southeast",
-	"Southeast",
-	"South-Southeast",
-	"South",
-	"South-Southwest",
-	"Southwest",
-	"West-Southwest",
-	"West",
-	"West-Northwest",
-	"Northwest",
-	"North-Northwest"
-};
+				"North", "North-Northeast", "Northeast", "East-Northeast",
+				"East", "East-Southeast", "Southeast", "South-Southeast",
+				"South", "South-Southwest", "Southwest", "West-Southwest",
+				"West", "West-Northwest", "Northwest", "North-Northwest"
+			};
+
 			int index = (int)Math.Round((degrees % 360) / 22.5);
 			return cardinalDirections[index % 16];
 		}
 	}
-
 }
